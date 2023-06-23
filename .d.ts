7293b995f45bf40d9d9d31cb0536d122882519b4
Mathew <mateusz.lu123@gmail.com>
2023-06-23 16:16:35 +0200

    fix

---
+++ .d.ts
@@ -0,0 +1,10 @@
+declare module 'googleapis' {
+    // Define a placeholder type for the 'googleapis' module
+    type GoogleApis = any;
+  
+    // Assign the placeholder type to the 'googleapis' module
+    const google: GoogleApis;
+  
+    // Export the 'google' object
+    export { google };
+}
